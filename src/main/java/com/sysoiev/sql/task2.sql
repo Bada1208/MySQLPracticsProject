aeb456ddcaeefe7cc70dc4f09cf6d7280ec848b5
Bada1208 <sysoievbogdan@gmail.com>
2020-08-22 18:01:51 +0200

    task 2 fixed

--- src/main/java/com/sysoiev/sql/task2.sql
+++ src/main/java/com/sysoiev/sql/task2.sql
@@ -4,10 +4,14 @@ Create table If Not Exists Employee
     Id     int,
     Salary int
 );
+drop table Employee;
 insert into Employee (Id, Salary)
 values (1, 100),
        (2, 200),
        (3, 300);
-select avg(Salary) as SecondHighestSalary from Employee;
+select Salary as SecondHighestSalary
+from Employee
+limit 1 offset 1;
+
 
 
